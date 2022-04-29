36e39c0016196b06065fa6003375e24cc9b9fd96
Shiyukine <benjbres@gmail.com>
2022-04-29 19:27:51 +0200

    changed smmothcurs

--- TouchTransporter/SmootherCursor/SmootherCursor.cs
+++ TouchTransporter/SmootherCursor/SmootherCursor.cs
@@ -11,7 +11,7 @@ namespace TouchTransporter.SmootherCursor
     {
         private static List<Point> points = new List<Point>();
         static List<Point> newPoints = new List<Point>();
-        public static bool enable = true;
+        public static bool enable = false;
 
         public static async void addPoint(double x, double y)
         {
@@ -37,16 +37,17 @@ namespace TouchTransporter.SmootherCursor
                         points[1] = tmp2;
                         points[2] = tmp3;
                         //
-                        Point i2 = points[0];
+                        Point i2 = points[2];
                         Point i1 = points[1];
-                        Point i0 = points[2];
+                        Point i0 = points[0];
                         /*Point i0 = new Point(400, 400);
                         Point i1 = new Point(500, 500);
                         Point i2 = new Point(400, 600);*/
-                        double nx = 2 * i1.X - i0.X / 2 - i2.X / 2;
-                        double ny = 2 * i1.Y - i0.Y / 2 - i2.Y / 2;
-                        //Vector v = new Vector(i1.X - i0.X, i1.Y - i0.Y);
-                        //Vector v2 = new Vector(i2.X - i1.X, i2.Y - i1.Y);
+                        Vector v1 = new Vector(i1.X - i0.X, i1.Y - i0.Y);
+                        Vector v2 = new Vector(i2.X - i1.X, i2.Y - i1.Y);
+                        Vector v = v1 + v1 / 2 + v2 / 2;
+                        double nx = i1.X + v.X;
+                        double ny = i1.Y + v.Y;
                         BezierCurve bc = new BezierCurve(i0, new Point(nx, ny), i2);
                         for (double i = 0.5; i < 1.0; i += 0.01)
                         {
